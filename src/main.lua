cfeb026e73dd32e241a702eeb3a5d462dc70b3e7
Nathan <nathanberry97@live.com>
2025-07-24 08:48:39 +0100

    adding files to setup the game structure

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,15 @@
 import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
+import "SceneManager"
+import "scenes/GameManager"
 
 local pd <const> = playdate
-local gfx <const> = playdate.graphics
+
+local sceneManager = SceneManager()
+sceneManager:switchScene(GameManager())
 
 function pd.update()
-    gfx.clear()
+    sceneManager:update()
+    sceneManager:draw()
 end
